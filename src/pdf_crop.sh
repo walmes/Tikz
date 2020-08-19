5d1e7789f4178cccfbc1ef0b634487130f558e51
Walmes Zeviani <walmes@ufpr.br>
2020-08-19 18:56:24 -0300

    Add function to crop pdf files.

---
+++ src/pdf_crop.sh
@@ -0,0 +1,3 @@
+pdfcrop --margins '5 10 3 -395' table_chi_squared.pdf table_chi_squared-crop.pdf
+pdfcrop --margins '5 10 -10 -395' table_F_snedecor.pdf table_F_snedecor-crop.pdf
+pdfcrop --margins '5 10 3 -335' table_t_student.pdf table_t_student-crop.pdf
